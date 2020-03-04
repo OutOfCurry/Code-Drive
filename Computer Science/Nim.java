e4da08d484467c44a37dae42d2f2347a26258710
OutOfCurry <61755173+OutOfCurry@users.noreply.github.com>
2020-03-03 21:02:02 -0500

    Add files via upload

--- Computer Science/Nim.java
+++ Computer Science/Nim.java
@@ -1,15 +1,22 @@
 import java.util.*;
 class Nim {
   public static void main(String [] args) {
+    // Scanner Class
     Scanner reader = new Scanner(System.in);
+    //Username Setup
     System.out.println("Player 1, Type in your username:");
     String player1 = reader.nextLine();
     System.out.println(player1);
     System.out.println("Player 2, Type in your username:");
     String player2 = reader.nextLine();
     System.out.println(player2);
-    int randomInteger = random.nextInt(10);// replace with random integer
-    System.out.println(randomInteger);
+    Random rand = new Random(); // random class creation
+    //random int creation for each Pilr
+    int A = 1 + rand.nextInt(10);
+    int B = 1 + rand.nextInt(10);
+    int C = 1 + rand.nextInt(10); 
+    System.out.print("A:" + A + " " + "B:" + B +" " + "C:" + C);//prints out piles
+  
     reader.close();
   }
 }
