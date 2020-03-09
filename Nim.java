b9e4aead9fd76fbcb3b5392a1174cbadac3ad83d
OutOfCurry <61755173+OutOfCurry@users.noreply.github.com>
2020-03-09 14:02:18 -0400

    Update Nim.java

--- Nim.java
+++ Nim.java
@@ -24,8 +24,12 @@ class Nim {
     int A = 1 + rand.nextInt(10);
     int B = 1 + rand.nextInt(10);
     int C = 1 + rand.nextInt(10); 
+     int counter1 = 0;
+     int counter2 = 0;
+    //boolean gamerun = true;
   
     while (true) {
+    
     // Game Initiation
     System.out.println(player1 + " Pick a pile:");
     //prints out piles
@@ -33,9 +37,8 @@ class Nim {
      System.out.println("Type the number According to its pile | A = 1 , B = 2 , C = 3");
     // User Input for Pile chooser
     int pile = reader.nextInt();
-    if (A == 0 && B == 0 && C == 0) {
-      break;
-    }
+    counter1++;
+
     // If Pile A is entered
     if (pile == 1) {
       System.out.println("Pick a number to remove from Pile A");
@@ -71,11 +74,29 @@ class Nim {
       else {
         System.out.println("Invalid.");
       }}
+    if ( A == 1 && B == 0 && C == 0) {
+      System.out.println(player2 + ", You have taken the L. Due to the unfortunate reasoning of your brain, " + player1 + " has won.");
+      break;
+    }
+     else if ( A == 0 && B == 1 && C == 0) {
+      System.out.println(player2 + ", You have taken the L. Due to the unfortunate reasoning of your brain, " + player1 + " has won.");
+      break;
+    }
+     else if  ( A == 0 && B == 0 && C == 1) {
+      System.out.println(player2 + ", You have taken the L. Due to the unfortunate reasoning of your brain, " + player1 + " has won.");
+      break;
+    }
+     else if (A == 0 && B == 0 && C == 0) {
+       System.out.println("Tie. Both of you have lost to the lack of intelligence stored inside your brain.");
+       break;
+     }
+
     
     // Player 2s turn
     System.out.println(player2 + " Pick a pile to remove a number from:");
     System.out.println("A:" + A + " " + "B:" + B +" " + "C:" + C);//prints out piles
     System.out.println("Type the number According to its pile | A = 1 , B = 2 , C = 3");
+    counter2++;
     
     // fixing the scanner bug 
     int pile2 = reader.nextInt();
@@ -116,14 +137,24 @@ class Nim {
         System.out.println("Invalid.");
       }
     }
-    else if ((A == 0) && (B == 0) && (C == 0)) {
+     //System.out.println(counter2 + "| " + counter1);
+       if ( A == 1 && B == 0 && C == 0) {
+      System.out.println(player1 + ", You have taken the L. Due to the unfortunate reasoning of your brain, " + player2 + " has won.");
       break;
     }
-    
- 
+     else if ( A == 0 && B == 1 && C == 0) {
+      System.out.println(player1 + ", You have taken the L. Due to the unfortunate reasoning of your brain, " + player2 + " has won.");
+      break;
     }
-    
-  }
+     else if  ( A == 0 && B == 0 && C == 1) {
+      System.out.println(player1 + ", You have taken the L. Due to the unfortunate reasoning of your brain, " + player2 + " has won.");
+      break;
+    }
+     else if (A == 0 && B == 0 && C == 0){
+       System.out.println("Tie. Both of you have lost to the lack of intelligence stored inside your brain.");
+       break;
+
+    }
 }
-
-
+}
+  }
